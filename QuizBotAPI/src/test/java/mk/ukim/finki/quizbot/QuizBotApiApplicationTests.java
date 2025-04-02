19efc91ebdd06294c53be41c27c00813a9bcb42e
Hristijan Zdravev <zdrhristijan@gmail.com>
2025-04-02 20:47:36 +0200

    initial commit

---
+++ QuizBotAPI/src/test/java/mk/ukim/finki/quizbot/QuizBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.quizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
